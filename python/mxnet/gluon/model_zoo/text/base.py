96d69db95628b47037b88293ad266b9847200d2a
Wang <wangcg.pku@gmail.com>
2018-03-07 21:11:28 -0800

    update base

--- python/mxnet/gluon/model_zoo/text/base.py
+++ python/mxnet/gluon/model_zoo/text/base.py
@@ -89,26 +89,17 @@ def get_rnn_cell(mode, num_layers, num_embed, num_hidden,
     return rnn_cell
 
 
-# def _apply_weight_drop_to_rnn_layer(block, rate, weight_dropout_mode = 'training'):
-#     params = block.collect_params('.*_h2h_weight')
-#     for key, value in params.items():
-#         weight_dropped_params = WeightDropParameter(value, rate, weight_dropout_mode)
-#         params._params[key] = weight_dropped_params
-#     for child_block in block._children:
+def _apply_weight_drop_to_rnn_layer(block, rate, weight_dropout_mode = 'training'):
+    params = block.collect_params('.*_h2h_weight')
+    for key, value in params.items():
+        weight_dropped_params = WeightDropParameter(value, rate, weight_dropout_mode)
+        params._params[key] = weight_dropped_params
+    for child_block in block._children:
+        _apply_weight_drop_to_rnn_layer(child_block, rate, weight_dropout_mode)
 #         child_block.collect_params('.*_h2h_weight')._params[key] = weight_dropped_params      
 #     block._unfused.params._params.clear()
 #     for _unfused_child_block in block._unfused:
 #         _unfused_child_block.collect_params('.*_h2h_weight')._params[key] = weight_dropped_params
-
-def _apply_weight_drop_to_rnn_layer(block, rate, weight_dropout_mode = 'training'):
-    params = block._params._params
-    for key, value in params.items():
-        if key[-11:] == '_h2h_weight':
-            weight_dropped_param = WeightDropParameter(value, rate, weight_dropout_mode)
-            params[key] = weight_dropped_param
-
-    for child_block in block._children:
-        _apply_weight_drop_to_rnn_layer(child_block, rate, weight_dropout_mode)
 
 
 def get_rnn_layer(mode, num_layers, num_embed, num_hidden, dropout, weight_dropout, weight_dropout_mode = 'training'):
