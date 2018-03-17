b364a90dae30148941e1a05147ac63063a36ba71
Sheng Zha <szha@users.noreply.github.com>
2018-03-16 17:19:28 -0700

    move DBatchEngine to batching.h (#7)
    move DBatchEngine to batching.h

---
+++ include/mxnet/batching.h
@@ -0,0 +1,82 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#ifndef MXNET_BATCHING_H_
+#define MXNET_BATCHING_H_
+
+#include <mxnet/op_attr_types.h>
+#include <mxnet/graph_attr_types.h>
+#include <nnvm/symbolic.h>
+#include <nnvm/op.h>
+#include <nnvm/graph.h>
+#include <vector>
+#include <utility>
+#include <string>
+#include <unordered_map>
+
+#include "./ndarray.h"
+
+namespace mxnet {
+
+class DBatchEngine {
+ public:
+  void SaveGraph(nnvm::Graph& g) {
+    graphs_.push_back(g);
+  }
+
+  const std::vector<nnvm::Graph>& Graphs() const {
+    return graphs_;
+  }
+
+  void Batch() {
+    // (TODO szha) batch and execution
+    LOG(INFO) << "batching, graphs size=" << graphs_.size();
+
+  }
+
+  void Fresh() {
+    graphs_.clear();
+  }
+
+  bool is_dbatch() const {
+    return is_dbatch_;
+  }
+
+  bool set_is_dbatch(bool is_dbatch) {
+    is_dbatch_ = is_dbatch;
+    return is_dbatch_;
+  }
+
+  static DBatchEngine* Get();
+ private:
+#if DMLC_CXX11_THREAD_LOCAL
+  static thread_local bool is_dbatch_;
+#else
+  static MX_THREAD_LOCAL bool is_dbatch_;
+#endif
+  std::vector<nnvm::Graph> graphs_;
+};
+
+nnvm::Graph BatchGraphs(std::vector<nnvm::Graph>& graphs) {
+
+}
+
+}
+
+#endif
