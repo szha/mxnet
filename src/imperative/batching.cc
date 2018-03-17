c41a27f488b5402d7bf2de19d2295d0c5adddbe8
Sheng Zha <zhasheng@amazon.com>
2018-03-16 23:49:32 -0700

    bookkeep outputs

--- src/imperative/batching.cc
+++ src/imperative/batching.cc
@@ -43,12 +43,16 @@ int64_t ComputeNodeSign(const nnvm::NodePtr& n) {
   return reinterpret_cast<int64_t>(n->op());
 }
 
-nnvm::Graph DBatchEngine::BatchGraphs(std::vector<nnvm::Graph>& graphs) {
+nnvm::Graph DBatchEngine::BatchGraphs(const std::vector<nnvm::Graph>& graphs) {
   // collect depth
   int max_depth = 0;
   std::unordered_map<nnvm::Node*, int> depth_map;
+  std::unordered_set<nnvm::NodeEntry, nnvm::NodeEntryHash, nnvm::NodeEntryEqual> prev_outputs;
   for (const nnvm::Graph& g : graphs) {
-    nnvm::DFSVisit(g.outputs, [&](const nnvm::NodePtr& n){
+    std::vector<nnvm::NodeEntry> g_outputs = g.outputs;
+    LOG(INFO) << "graph outputs " << g_outputs.size();
+    std::copy(g_outputs.begin(), g_outputs.end(), std::inserter(prev_outputs, prev_outputs.end()));
+    nnvm::DFSVisit(g_outputs, [&](const nnvm::NodePtr& n){
       int depth = 0;
       for (auto e : n->inputs) {
         int idepth = depth_map.at(e.node.get());
@@ -59,28 +63,28 @@ nnvm::Graph DBatchEngine::BatchGraphs(std::vector<nnvm::Graph>& graphs) {
       max_depth = std::max(depth, max_depth);
     });
   }
+  LOG(INFO) << "total graph outputs " << prev_outputs.size();
 
   LOG(INFO) << "max depth " << max_depth;
   // depth: sign->node
-  std::vector<std::unordered_map<int64_t, nnvm::NodePtr>> infos;
-  infos.resize(max_depth + 1);
+  std::vector<std::unordered_map<int64_t, std::vector<nnvm::NodePtr>>> forward_steps;
+  forward_steps.resize(max_depth + 1);
 
   for (const nnvm::Graph& g : graphs) {
     nnvm::DFSVisit(g.outputs, [&](const nnvm::NodePtr& n){
       int64_t sign = ComputeNodeSign(n);
       int depth = depth_map.at(n.get());
-      infos[depth].insert({sign, n});
+      forward_steps[depth][sign].emplace_back(n);
     });
   }
 
   // generate new graph
   // (TODO szha)
 
-
   return nnvm::Graph();
 }
 
-void DBatchEngine::ExecuteGraph(nnvm::Graph& graph) {
+void DBatchEngine::ExecuteGraph(const nnvm::Graph& graph) {
   // TODO
 }
 
