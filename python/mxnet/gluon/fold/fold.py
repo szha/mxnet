ee770d2bdf52d079e71d4fd5cb77eb709afe7737
ZihengJiang <ziheng@apache.org>
2018-10-15 01:28:49 +0000

    [FOLD] Simple batching implementation

--- python/mxnet/gluon/fold/fold.py
+++ python/mxnet/gluon/fold/fold.py
@@ -22,13 +22,22 @@ __all__ = ['batching', 'is_batching', 'NDArrayFuture', 'create_ndarray_future']
 
 import numpy as np
 import ctypes
-import collections
 import random
 import sys
+from collections import namedtuple, defaultdict
 
 from ... import ndarray
 from ...base import check_call, _LIB, py_str
 from ...ndarray import NDArray
+
+# TODO
+# - improve ndarray future
+# - op signature
+# - infer batch axis
+# - multiple outputs
+# - optimize batch algorithm
+
+_OpRecord = namedtuple('OpRecord', ['op_name', 'future', 'inputs', 'attrs'])
 
 
 class NDArrayFutureManager(object):
@@ -60,14 +69,14 @@ class NDArrayFuture(NDArray):
         self.manager = get_future_manager()
 
     def __del__(self):
-        print('delete future')
+        # do nothing to avoid double free
+        pass
 
     def __repr__(self):
         if self.instantiated:
             return NDArray.__repr__(self)
         else:
-            return '<NDArrayFuture(uninstantiated)>'
-
+            return '<NDArrayFuture (uninstantiated) @ {0}>'.format(self.key)
 
     def instantiate(self, arr):
         assert isinstance(arr, NDArray)
@@ -76,7 +85,6 @@ class NDArrayFuture(NDArray):
         self.instantiated = True
 
     def __getattribute__(self, attr):
-        # print('get attr: {0}'.format(attr))
         if attr in NDArrayFuture.allowed:
             return NDArray.__getattribute__(self, attr)
 
@@ -92,7 +100,6 @@ class NDArrayFuture(NDArray):
         else:
             return NDArray.__getattribute__(self, attr)
 
-
 def create_ndarray_future(arr=None):
     future = NDArrayFuture()
     if arr is not None:
@@ -107,60 +114,81 @@ def calculate_signature(op_name, args, kwargs):
 # algorithm part for dynamic batching
 class Fold(object):
     def __init__(self):
-        self.steps = collections.defaultdict(
-            lambda: collections.defaultdict(list))
+        self.max_step = 0
+        self.steps = defaultdict(lambda: defaultdict(list))
         self.depths = {}
         self.exec_list = []
-        self.out_futures = []
 
-    def record(self, op_name, out, args, kwargs):
-        # print('op_name: {0}'.format(op_name))
-        # print('args: {0}'.format(args))
-        # print('kwargs: {0}'.format(kwargs))
+    def record(self, op_name, future, inputs, attrs):
         # setup input depth
-        for arg in args:
+        for arg in inputs:
             if arg not in self.depths:
                 # NDArray or instantiated NDArrayFuture
                 if isinstance(arg, NDArrayFuture):
                     assert arg.instantiated
                 self.depths[arg] = 0
-        step = max([self.depths[arg] for arg in args]) + 1
-        self.depths[out] = step
-        op_tuple = (op_name, args, kwargs)
-        op_sig = calculate_signature(op_name, args, kwargs)
-        self.steps[step][op_sig].append(op_tuple)
-        self.exec_list.append(op_tuple)
-        self.out_futures.append(out)
 
+        step = max([self.depths[arg] for arg in inputs]) + 1
+        self.max_step = max(step, self.max_step)
+        self.depths[future] = step
+        op_sig = calculate_signature(op_name, inputs, attrs)
+        self.steps[step][op_sig].append(_OpRecord(op_name, future, inputs, attrs))
+
+    def _concat_inputs(self, inputs):
+        concat_attrs = {'dim': 0}
+        concat_out = create_ndarray_future()
+        concat_op = _OpRecord('concat', concat_out, inputs, concat_attrs)
+        return concat_out, concat_op
+
+    def _split_output(self, new_op_out, futures, num_batches):
+        indices = [0]
+        acc = 0
+        for num_batch in num_batches:
+            acc += num_batch
+            indices.append(acc)
+        split_ops = []
+        for i in range(1, len(indices)):
+            split_attrs = {'axis': 0, 'begin': indices[i - 1], 'end': indices[i]}
+            split_op = _OpRecord('slice_axis', futures[i - 1], [new_op_out], split_attrs)
+            split_ops.append(split_op)
+        return split_ops
 
     def batch(self):
-        for step in range(max_step):
+        # TODO replace other inputs with changed output
+        for step in range(self.max_step + 1):
             for op_sig in self.steps[step]:
-                # exec concatenate
                 old_ops = self.steps[step][op_sig]
-                new_inputs, concat_op = concat_inputs(old_ops)
-                self.exec_list.append(concat_op)
-                outputs, new_op = (old_ops[0][0], new_inputs, old_ops[0][2])
-                self.exec_list.append(new_op)
-                new_outputs, split_op = split_outputs(outputs)
-                self.exec_list.append(split_op)
+                op_name = old_ops[0].op_name
+                futures = [op.future for op in old_ops]
+                inputs = [op.inputs[0] for op in old_ops]
+                params = old_ops[0].inputs[1:]
+                attrs = old_ops[0].attrs
 
+                concat_out, concat_op = self._concat_inputs(inputs)
+                self.exec_list.append(concat_op)
+
+                new_op_in = (concat_out, ) + params
+                new_op_out = create_ndarray_future()
+                new_op = _OpRecord(op_name, new_op_out, new_op_in, attrs)
+                self.exec_list.append(new_op)
+
+                # TODO no shape info before execution
+                num_batches = [16] * len(old_ops)
+                split_ops = self._split_output(new_op_out, futures, num_batches)
+                self.exec_list += split_ops
 
     def execute(self):
-        for (op_name, args, kwargs), future in zip(self.exec_list, self.out_futures):
-            op = getattr(ndarray, op_name)
-            # print(op)
-            # print('args: {0}'.format(args))
-            out = op(*args, **kwargs)
-            future.instantiate(out)
-            # print(future)
-
+        # print('exec list')
+        # for op in self.exec_list:
+        #     print(op)
+        for record in self.exec_list:
+            op = getattr(ndarray, record.op_name)
+            out = op(*record.inputs, **record.attrs)
+            record.future.instantiate(out)
 
     def clear(self):
         pass
 
-
-# batching scope manager
 
 _CurrentBatchingScope = None
 
@@ -178,60 +206,26 @@ def is_batching():
     return (_current_batching_scope() is not None)
 
 class _BatchingScope(object):
+    """batching scope manager """
     def __init__(self, batch_size):
         self._fold = Fold()
         self._batch_size = batch_size
 
     def __enter__(self):
-        print('enter batching scope')
         if _current_batching_scope() is not None:
             raise ValueError, "nested batching scope is not allowed"
         _set_current_batching_scope(self)
 
     def __exit__(self, ptype, value, trace):
-        print('exit batching scope')
-        # real execution
+        # real execution happens
         # compute all NDArrayFuture
-        # self._fold.batch()
+        self._fold.batch()
         self._fold.execute()
         self._fold.clear()
         _set_current_batching_scope(None)
 
     def record(self, op, out, args, kwargs):
         self._fold.record(op, out, args, kwargs)
-
-
-def _split_batch(arg, batch_axis, arg_size):
-    if isinstance(arg, nd.NDArray):
-        return nd.split(arg, arg_size, axis=batch_axis) if arg_size > 1 else (arg,)
-    arg, fmt = _flatten(arg)
-    if arg_size > 1:
-        result = (nd.split(x, arg_size, axis=batch_axis) for x in arg)
-    else:
-        result = ((x,) for x in arg)
-    result = zip(*result)
-    out = [_regroup(x, fmt)[0] for x in result]
-    return out
-
-
-def _batch_args(arg_lists, arg_types, values, batch_axis):
-    res = []
-    arg_size = 1
-    for arg, arg_type in zip(arg_lists, arg_types):
-        arg_size = len(arg)
-        if arg_type[0] == 1 and arg[0].batch:
-            out = nd.concat(*(x(values) for x in arg), dim=batch_axis)
-        elif arg_type[0] == -1:
-            out = nd.concat(*arg, dim=batch_axis)
-        else:
-            for i in range(2, len(arg)):
-                assert arg[i] == arg[0], \
-                    "Can not use more than one of no-batch argument, got: %s." % str(arg)
-            out = arg[0]
-            if arg_type[0] == 1:
-                out = out(values)
-        res.append(out)
-    return tuple(res), arg_size
 
 
 def _make_op_func(name, func_name):
@@ -245,9 +239,6 @@ def {0}(*args, **kwargs):
     return future
     """.format(func_name)
     doc_str = ""
-
-    if name == "FullyConnected":
-        print(code)
 
     local = {}
     exec(code, None, local)  # pylint: disable=exec-used
@@ -268,7 +259,6 @@ def _init_ops():
         op_names.append(py_str(plist[i]))
 
     for name in op_names:
-        # print(name)
         func_name = name
         function = _make_op_func(name, func_name)
         cur_module = sys.modules['mxnet.gluon.fold.op']
